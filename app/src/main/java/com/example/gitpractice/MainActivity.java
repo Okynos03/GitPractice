a56170b9a75f31030ac2c873056be4b0f9753af1
Uli <ulises.ag03@gmail.com>
2023-10-31 20:21:25 -0600

    Revert "This is our third commit"
    This reverts commit 52d558f4e621af4ed678a10b5568e30a19494868.

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("THIS IS MY SECOND COMMIT");
-        today is a good  day
     }
 }
